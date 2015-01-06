197e4c865aefd9399818103ac090ae8fe86257bc
Compudoctors <tauseefahmad@rocketmail.com>
2015-01-06 09:46:29 +0530

    Content Added for main Page

--- scripts/inhousescripts/indexScript.js
+++ scripts/inhousescripts/indexScript.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-    $('.menu').click(function(){
-        alert('Clicked');
-    });
+$(document).ready(function () {
+  toggleContentDisplay('mainPage');
+  //$('#imageContainer').html('');
+  $('.menu').click(function () {
+    var $container = $(this);
+    toggleContentDisplay($container.attr('data-attr-content'));
+  });
 });
+
+function toggleContentDisplay(id) {
+  $('.content').hide();
+  $('#'+ id +'').show();
+}
